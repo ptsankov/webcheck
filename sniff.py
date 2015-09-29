1708a802dc434a2795598d7ec18ff7e253e1c62c
Petar Tsankov <petar.tsankov@gmail.com>
2015-09-29 20:15:13 +0200

    i.

--- sniff.py
+++ sniff.py
@@ -1,7 +1,26 @@
 #!/usr/bin/python
 
+from threading import Thread
 from scapy.all import *
 import sys
+import subprocess
 
-requests = sniff(iface="eth0", filter="tcp and port 80 and src 192.33.93.236", count=20000)
-wrpcap(sys.argv[1], requests)
+filter_conf = "tcp and port 80"
+interface = "eth0"
+arachni = '/opt/arachni-1.2.1-0.5.7.1/bin/arachni'
+
+def log_testcase(output_filename, num_packets):
+  print 'Logging test case to', output_filename
+  requests = sniff(iface=interface, filter=filter_conf, count=num_packets)
+  wrpcap(output_filename, requests)
+
+for i in range(0, 10):
+  print 'Test case', i
+  test_filename = "test_" + str(i)
+  Thread(target = log_testcase, args = (test_filename, 100))
+  
+  cmd = arachni + " http://bellog.org/cuteflow/index.php --checks=* --plugin=autologin:url=http://bellog.org/cuteflow/index.php,parameters='UserId=admin&Password=admin',check='.' --scope-exclude-pattern logout"
+  #cmd = "sleep 2" 
+  print cmd
+  process = subprocess.Popen(cmd.split(' '))
+  output = process.communicate()
