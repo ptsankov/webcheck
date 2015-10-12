e843a49b7408ba6a87d34108e15f7489bff1b7e7
Petar Tsankov <petar.tsankov@gmail.com>
2015-10-12 23:50:52 +0200

    ignore

--- generate_tests.py
+++ generate_tests.py
@@ -13,14 +13,17 @@ def generate_test(output_filename, test_length):
     assert os.path.isfile(w3af_script)
     if os.path.isfile(output_filename):
         msg('The output file {} exists, remove it.'.format(output_filename))
+        sys.exit(-1)
     msg('Logging test to: {}'.format(output_filename))
+    msg('Test case length: {} {} {}'.format(test_length, NET_IFACE, HTTP_FILTER))
     requests = sniff(iface=NET_IFACE, filter=HTTP_FILTER, count=test_length)   
+    msg('Done sniffing')
     wrpcap(output_filename, requests) 
 
 if __name__ == "__main__":
     global w3af_script, num_tests, test_length
     if len(sys.argv) != 4:
-        msg('Usage: ' + sys.argv[0] + ' <w3af script> <num_tests> <test case length>')
+        msg('Usage: ' + sys.argv[0] + ' <w3af script> <number of test cases> <test case length>')
         sys.exit(-1)
     
     w3af_script = sys.argv[1]    
@@ -34,7 +37,7 @@ if __name__ == "__main__":
     for i in range(0, num_tests):
         msg('Generating test case {}'.format(i))
         test_filename = 'test{}.{}'.format(i, PACKET_FILE_EXTENSION)
-        cmd = '{} --script={}'.format(W3AF, w3af_script)
+        cmd = 'sudo {} --script={}'.format(W3AF, w3af_script)
         #cmd = "sleep 2"
         
         Thread(target=generate_test, args=(test_filename, test_length)).start()
