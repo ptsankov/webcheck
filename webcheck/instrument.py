779d3f5df6f85bcb85fac3ec1fb5a6396d56b9d5
Petar Tsankov <petar.tsankov@gmail.com>
2016-03-02 17:59:16 +0100

    update code

--- webcheck/instrument.py
+++ webcheck/instrument.py
@@ -5,7 +5,193 @@ import ConfigParser
 from static import INSTRUMENTATION_SECTION, PROXY_SECTION
 
 
-def php_instrumentation(proxy):
+def php_instrumentation():
+    if sqlFlag:
+        sql_instrumentation = instrument_sql_api()
+    else:
+        sql_instrumentation = ""
+      
+    if fileFlag:
+        file_instrumentation = instrument_file_api()
+    else:
+        file_instrumentation = ""
+      
+    if randomFlag:
+        random_instrumentation = instrument_random_api()
+    else:
+        random_instrumentation = ""
+      
+    if timeFlag:
+        time_instrumentation = instrument_time_api()
+    else:
+        time_instrumentation = ""
+    return sql_instrumentation + file_instrumentation + random_instrumentation + time_instrumentation
+  
+  
+def instrument_file_api():  
+    return '''              
+            if (!function_exists('fopen_old')) {
+              runkit_function_copy ( 'fopen', 'fopen_old' );
+              
+              runkit_function_redefine('fopen', '$filename , $mode , $use_include_path = false', '// Modified fopen()                            
+		  $log = fopen_old ("''' + fileLogPath + '''", "a");
+		  fwrite($log, $filename . "\\r\\n");
+		  fclose($log);
+		  $result = fopen_old ($filename, $mode, $use_include_path);
+		  return $result;
+                '); 
+            }            
+            // -------------
+            '''  
+  
+  
+def instrument_random_api():  
+    return '''              
+            if (!function_exists('rand_old')) {
+              runkit_function_copy ( 'rand', 'rand_old' );
+              
+              runkit_function_redefine('rand', '$min = 0, $max = 0', '// Modified rand()
+		  // read seed
+		  $seedFile = fopen_old("''' + randomSeedPath + '''", "r");
+		  $seed = (int) fgets($seedFile);
+		  fclose($seedFile);
+		  // read currentValue
+		  $currentValPath = fopen_old("''' + randomSnapshotPath + '''", "r");
+		  $currentVal= (int) fgets($currentValPath);
+		  fclose($currentValPath);
+		  // compute new value
+		  $newRandomVal = $currentVal+1;//(($seed + 2) * $currentVal + ($seed+1)) % ($seed + 3);
+		  // update currentValue
+		  $currentValPath = fopen_old("''' + randomSnapshotPath + '''", "w");
+		  fwrite($currentValPath, $newRandomVal);
+		  fclose($currentValPath);
+		  if($min == 0 && $max == 0)
+		  {
+		    return $newRandomVal;
+		  }
+		  else {
+		    return $newRandomVal % $max;
+		  }
+                '); 
+            }            
+            // -------------
+            if (!function_exists('mt_rand_old')) {
+              runkit_function_copy ( 'mt_rand', 'mt_rand_old' );
+              
+              runkit_function_redefine('mt_rand', '$min = 0, $max = 0', '// Modified mt_rand()
+		  return rand($min, $max);
+                '); 
+            }             
+            // -------------
+            if (!function_exists('uniqid_old')) {
+              runkit_function_copy ( 'uniqid', 'uniqid_old' );
+              
+              runkit_function_redefine('uniqid', '$prefix = "", $more_entropy = false', '// Modified uniqid()
+		  return  $prefix."AAAA";
+                '); 
+            }            
+            // ------------- 
+            if (!function_exists('session_id_old')) {
+              runkit_function_copy ( 'session_id', 'session_id_old' );
+              
+              runkit_function_redefine('session_id', '$id = ""', '// Modified session_id()              
+		  // read currentValue
+		  $sessionValuePath = fopen_old("''' + sessionSnapshotPath + '''", "r");
+		  $session_id= (int) fgets($sessionValuePath);
+		  fclose($sessionValuePath);
+		  if ($id != "")
+                        session_id_old($id);
+		  return md5($session_id);
+                '); 
+            }            
+            // ------------- 
+	    if (!function_exists('session_regenerate_id_old')) {
+              runkit_function_copy ( 'session_regenerate_id', 'session_regenerate_id_old' );
+              
+              runkit_function_redefine('session_regenerate_id', '$delete_old_session = false', '// Modified session_regenerate_id()              
+		  // read currentValue
+		  $sessionValuePath = fopen_old("''' + sessionSnapshotPath + '''", "r");
+		  $session_id= (int) fgets($sessionValuePath);
+		  fclose($sessionValuePath);
+		  // compute new value
+		  $new_session_id = $session_id+1;
+		  // delete old value if needed
+                  session_regenerate_id_old($delete_old_session);
+                  // set new value in the cookie
+                  session_id_old($new_session_id);
+		  // update currentValue
+		  $sessionValuePath = fopen_old("''' + sessionSnapshotPath + '''", "w");
+		  fwrite($sessionValuePath, $new_session_id);
+		  fclose($sessionValuePath);
+		  return  true;
+                '); 
+            }            
+            // -------------  
+            if (!function_exists('session_start_old')) {
+              runkit_function_copy ( 'session_start', 'session_start_old' );
+
+              runkit_function_redefine('session_start', ' $options = [] ', '// Modified session_start()              
+                  $newId = session_id(); // get new link_identifier
+                  session_id_old($newId); // set new identifier before the start
+                  session_start_old($options); // actually start the session         
+                ');
+            }
+            // ------------- 
+            '''  
+    
+def instrument_time_api():  
+    return '''              
+            if (!function_exists('time_old')) {
+              runkit_function_copy ( 'time', 'time_old' );
+              
+              runkit_function_redefine('time', '', '// Modified time()
+		  // read seed
+		  $seedFile = fopen_old("''' + timeSeedPath + '''", "r");
+		  $seed = (int) fgets($seedFile);
+		  fclose($seedFile);
+		  // read counter
+		  $counterFile = fopen_old("''' + timeSnapshotPath + '''", "r");
+		  $counter= (int) fgets($counterFile);
+		  fclose($counterFile);
+		  // update counter
+		  $counterFile = fopen_old("''' + timeSnapshotPath + '''", "w");
+		  fwrite($counterFile, $counter+1);  
+		  fclose($counterFile);
+		  $result = $seed + $counter;
+		  return $result;
+                '); 
+            }            
+            // -------------
+            if (!function_exists('microtime_old')) {
+              runkit_function_copy ( 'microtime', 'microtime_old' );
+              
+              runkit_function_redefine('microtime', '$get_as_float = false', '// Modified microtime()
+		  // read seed
+		  $seedFile = fopen_old("''' + timeSeedPath + '''", "r");
+		  $seed = (int) fgets($seedFile);
+		  fclose($seedFile);
+		  // read counter
+		  $counterFile = fopen_old("''' + timeSnapshotPath + '''", "r");
+		  $counter= (int) fgets($counterFile);
+		  fclose($counterFile);
+		  // update counter
+		  $counterFile = fopen_old("''' + timeSnapshotPath + '''", "w");
+		  fwrite($counterFile, $counter+1);  
+		  fclose($counterFile);
+		  if ($get_as_float) {
+		    $result = (float) $seed + $counter;
+		  }
+		  else {
+		    $result = strval($seed) . " " . strval($counter);
+		  }
+		  return $result;
+                '); 
+            }
+            // -------------
+            ''' 
+    
+  
+def instrument_sql_api(): 
     return '''              
             if (!function_exists('mysql_query_old')) {
               runkit_function_copy ( 'mysql_query', 'mysql_query_old' );
@@ -15,32 +201,17 @@ def php_instrumentation(proxy):
                     print("an update query");
                  //--------------- Connect to proxy ----------------
                  $client = stream_socket_client("tcp://''' + proxy + '''", $errno, $errorMessage);
-              
                  if ($client === false) {
                   throw new UnexpectedValueException("Failed to connect: $errorMessage");
                  }
-              
                  fwrite($client, $query);
-              
                  // Get response from proxy.
                  $response = stream_get_contents($client);
-              
-                  #if ($response == "true" or $response == "false") {
                   if ($response == "true") {
                       $result = TRUE;
                   } else {
                       $result = FALSE;
                   }
-                  #} 
-                  #else {                  
-                    # $newQuery = $response;
-                    # if (is_null($link_identifier)) {
-                    #   $result = mysql_query_old ( $newQuery);
-                    # } else {
-                    #   $result = mysql_query_old ( $newQuery, $link_identifier);
-                    # }
-                  #}
-              
                  // Close connection to proxy.
                  fclose($client);
                 } else {
@@ -50,10 +221,40 @@ def php_instrumentation(proxy):
                 '); 
             }            
             // -------------
+            if (!function_exists('mysqli_query_old')) {
+              runkit_function_copy ( 'mysqli_query', 'mysqli_query_old' );
+              
+              runkit_function_redefine('mysqli_query', ' $link_identifier, $query', '// Modified mysql_query()                
+                if (stripos($query, "UPDATE") === 0 || stripos($query, "INSERT") === 0 || stripos($query, "DELETE") === 0) {
+                    print("an update query");
+                 //--------------- Connect to proxy ----------------
+                 $client = stream_socket_client("tcp://''' + proxy + '''", $errno, $errorMessage);
+                 if ($client === false) {
+                  throw new UnexpectedValueException("Failed to connect: $errorMessage");
+                 }
+                 fwrite($client, $query);
+                 // Get response from proxy.
+                 $response = stream_get_contents($client);
+                  if ($response == "true") {
+                      $result = TRUE;
+                  } else {
+                      $result = FALSE;
+                  }
+                 // Close connection to proxy.
+                 fclose($client);
+                } else {
+                  $result = mysqli_query_old ($link_identifier, $query);
+                }
+                 return $result;
+                '); 
+            }            
+            // -------------
             '''
 
 
 if __name__ == "__main__":
+  
+    global sqlFlag, fileFlag, randomFlag, timeFlag, proxy, fileLogPath, randomSnapshotPath, timeSnapshotPath, randomSeedPath, timeSeedPath, sessionSnapshotPath
     
     if len(sys.argv) != 2:
         print ('Usage: ' + sys.argv[0] + ' <config file>')
@@ -64,15 +265,27 @@ if __name__ == "__main__":
     config.read(config_file)
     app_path = config.get(INSTRUMENTATION_SECTION, 'PATH')
     
+    
+    sqlFlag = config.getboolean(INSTRUMENTATION_SECTION, 'SQL_INSTRUMENTATION')
     proxy_ip = config.get(PROXY_SECTION, 'IP')
     proxy_port = config.getint(PROXY_SECTION, 'PORT')
     proxy = '{}:{}'.format(proxy_ip, proxy_port)
-
     
+    fileFlag = config.getboolean(INSTRUMENTATION_SECTION, 'FILE_INSTRUMENTATION')
+    fileLogPath = config.get(INSTRUMENTATION_SECTION, 'ACCESSED_FILES_LOG_PATH')
+    
+    randomFlag = config.getboolean(INSTRUMENTATION_SECTION, 'RANDOM_INSTRUMENTATION')
+    randomSnapshotPath = config.get(INSTRUMENTATION_SECTION, 'RANDOM_SNAPSHOT_PATH')
+    randomSeedPath = config.get(INSTRUMENTATION_SECTION, 'RANDOM_SEED_PATH')
+    sessionSnapshotPath = config.get(INSTRUMENTATION_SECTION, 'RANDOM_SESSION_SNAPSHOT_PATH')
+    
+    timeFlag = config.getboolean(INSTRUMENTATION_SECTION, 'TIME_INSTRUMENTATION')
+    timeSnapshotPath = config.get(INSTRUMENTATION_SECTION, 'TIME_SNAPSHOT_PATH')  
+    timeSeedPath = config.get(INSTRUMENTATION_SECTION, 'TIME_SEED_PATH')
         
     phpFiles = [os.path.join(dp, f) for dp, dn, filenames in os.walk(app_path) for f in filenames if os.path.splitext(f)[1] == '.php']
     
-    
+  
     for phpFile in phpFiles:
         print phpFile
         content = None
@@ -83,12 +296,12 @@ if __name__ == "__main__":
         with open(phpFile, "wt") as fout:
             instrumented = False
             for line in content:
-                if not instrumented and (not has_namespace) and line.startswith('<?php'):
-                    fout.write(line.replace('<?php', '<?php\n' + php_instrumentation(proxy)))
+                if not instrumented and (not has_namespace) and line.strip().startswith('<?php'):
+                    fout.write(line.replace('<?php', '<?php\n' + php_instrumentation()))
                     instrumented = True
-                elif not instrumented and has_namespace and line.startswith('namespace'):
+                elif not instrumented and has_namespace and line.strip().startswith('namespace'):
                     fout.write(line)
-                    fout.write(php_instrumentation(proxy))
+                    fout.write(php_instrumentation())
                     instrumented = True
                 else:
                     fout.write(line)
--- webcheck/webcheck.py
+++ webcheck/webcheck.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python
 import sys
-from static import TESTS_SECTION, EXECUTION_SECTION, APPLICATION_SECTION, PROXY_SECTION, DATABASE_SECTION
+from static import TESTS_SECTION, EXECUTION_SECTION, APPLICATION_SECTION, PROXY_SECTION, DATABASE_SECTION, INSTRUMENTATION_SECTION
 import ConfigParser
 import glob
 from os import path
 import socket
 import requests
 import shlex
-from subprocess import PIPE, Popen
-import MySQLdb
 import time
+from subprocess import PIPE, Popen
+#import MySQLdb
+import time
+from collections import OrderedDict
+from symbol import parameters
+import random
 
 def output(line):
     print 'OUTPUT: {}'.format(line)
@@ -22,6 +26,7 @@ def parse_test_file(file):
         request = None
         in_request = False
         for line in content:
+            line = line.replace('bellog.org', application_ip)
             if line.startswith('========================================Response'):
                 in_request = False    
                 if request.startswith('GET'):
@@ -29,8 +34,6 @@ def parse_test_file(file):
                 requests.append(request)
             if in_request and not line.startswith('Cookie'):
                 request += line.strip() + '\r\n'
-            if in_request and line.startswith('Cookie'):
-                request += 'Cookie: {}\r\n'.format(cookie)
             if line.startswith('========================================Request'):
                 in_request = True
                 request = ''            
@@ -78,11 +81,11 @@ def filter_test(test, filter_extensions, allowed_urls):
 def read_tests(path_to_tests):
     tests = []
     for test_file in glob.glob(path.join(path_to_tests, '*_http.txt')):
-        print test_file        
         test = parse_test_file(test_file)
-        allowed_urls = white_list(test_file.replace('http', 'log')) 
+        allowed_urls = white_list(test_file.replace('http', 'log'))
         filtered_test = filter_test(test, filter_extensions, allowed_urls)[:test_length]
         tests.append(filtered_test)
+    print 'read', len(tests), 'tests'
     return tests
         
         
@@ -106,12 +109,30 @@ def runcmd(cmd, input=PIPE, output=PIPE, cwd=None):
     return out
 
 def checkpoint(label):
+    global current_cookie
     start_time = time.time()
+    
+    
+    cookies[label] = current_cookie        
+    print 'Checkpointing cookie:', current_cookie, 'for label', label
     if checkpointing:
+        labels_to_files[label] = {}
+        labels_to_files[label][randomSnapshotPath] = open(randomSnapshotPath, 'r').readlines()[0]
+        labels_to_files[label][randomSeedPath] = open(randomSeedPath, 'r').readlines()[0]
+        labels_to_files[label][timeSnapshotPath] = open(timeSnapshotPath, 'r').readlines()[0]
+        labels_to_files[label][timeSeedPath] = open(timeSeedPath, 'r').readlines()[0]
+        labels_to_files[label][sessionSnapshotPath] = open(sessionSnapshotPath, 'r').readlines()[0]
+        
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((proxy_ip, proxy_port))
         sock.sendall('checkpoint:{}'.format(label))
-        print 'Checkpointing', label, sock.recv(2)
+        print 'Checkpointing', label, sock.recv(2)        
+    elif vm:
+        runcmd('VBoxManage snapshot webcheck take {} --live'.format(label + '_' + timestamp))
+        runcmd('ssh guestserver.com cat /var/www/html/oscommerce/random_snapshot.txt')
+        runcmd('ssh guestserver.com cat /var/www/html/oscommerce/time_snapshot.txt')
+        print "Sleep for 5 seconds"
+        time.sleep(5)
     else:
         dump_file = 'checkpoint_{}'.format(label)
         #runcmd('mysqldump -u {} -p{} {} {}'.format(db_username, db_password, db_database, ' '.join(table_names)), output=open(dump_file, 'w'))
@@ -133,18 +154,39 @@ def get_table_names():
 
     
 def restore(label):
+    global current_cookie
     start_time = time.time()
+    current_cookie = cookies[label]
+    print 'Restored cookie', current_cookie
     print 'restoring'
+    
+    
     if checkpointing:
+        
+        
+        for f in labels_to_files[label].keys():
+            fd = open(f, 'w')
+            fd.write(labels_to_files[label][f])
+            fd.close()
+        
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((proxy_ip, proxy_port))
         sock.sendall('restore:{}'.format(label))
         print 'Restoring', label, sock.recv(2)
+    elif vm:
+        runcmd('VBoxManage controlvm webcheck poweroff')
+        runcmd('VBoxManage snapshot webcheck restore {}'.format(label + '_' + timestamp))
+        runcmd('VBoxManage startvm webcheck --type headless')
+        print "Sleep for 30 seconds"
+        time.sleep(30)
+        runcmd('ssh guestserver.com cat /var/www/html/oscommerce/random_snapshot.txt')
+        runcmd('ssh guestserver.com cat /var/www/html/oscommerce/time_snapshot.txt')
     else:
         dump_file = 'checkpoint_{}'.format(label)
         runcmd('mysql -u {} -p{} {}'.format(db_username, db_password, db_database, dump_file), input='source {}'.format(dump_file))
     print 'done restoring'
     end_time = time.time()
+
     print 'Restore time:', end_time - start_time
 
 def parse_request(request):
@@ -157,23 +199,61 @@ def parse_request(request):
         headers[key] = value
     return (url, headers)
 
-def execute_request(request):
-    print request
+def execute_request(request, test_id, counter):
+    global current_cookie    
     if request.startswith('GET'):
         (url, headers) = parse_request(request)
-        response = requests.get(url, headers=headers)
+        if current_cookie is not None:
+            headers['Cookie'] = current_cookie
+        print 'GET', url
+        print '\n'.join([str(k) + ':' + headers[k] for k in headers.keys()])
+        print '\n'
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if 'set-cookie' in response.headers.keys():
+            cookies = response.headers['set-cookie']
+            current_cookie = '; '.join([cookie.split(';')[0] for cookie in cookies.split('\n')])
+            print 'current_cookie', current_cookie
+            #print 'reexecute the request'
+            #execute_request(request, test_id, counter)             
         print response.status_code
     else:
         (url, headers) = parse_request(request.split('\r\n\r\n')[0])
+        if current_cookie is not None:
+            headers['Cookie'] = current_cookie            
         if (request.split('\r\n\r\n')) > 0:
             key_value_pairs = request.split('\r\n\r\n')[1].strip()
-            parameters = {}
-            for key_value in key_value_pairs.split('&'):
-                key = key_value.split('=')[0]
-                value = key_value.split('=')[1]
-                parameters[key] = value
-        response = requests.post(url, headers=headers, data=parameters)
+            #parameters = OrderedDict()
+            parameters = key_value_pairs
+            #for key_value in key_value_pairs.split('&'):
+            #    key = key_value.split('=')[0]
+            #    value = key_value.split('=')[1]
+            #    parameters[key] = value
+        print 'POST', url
+        print '\n'.join([str(k) + ':' + headers[k] for k in headers.keys()])
+        print parameters
+        print '\n'
+        response = requests.post(url, headers=headers, data=parameters, allow_redirects=False)
+        if 'set-cookie' in response.headers.keys():
+            cookies = response.headers['set-cookie']
+            current_cookie = '; '.join([cookie.split(';')[0] for cookie in cookies.split('\n')])
+            print 'current_cookie', current_cookie
+            #print 'reexecute the request'
+            #execute_request(request, test_id, counter)             
         print response.status_code
+    if log:
+        out = open(response_log + '_' + str(test_id) + '_' + str(counter) + '.log', 'w')
+        out.write('\n'.join([str(k) + ':' + response.headers[k] for k in response.headers.keys()]))
+        out.write(response.content)
+        out.close()
+        if vm:
+            cmd = 'ssh {} mysqldump -u {} -p{} {} --result-file=/tmp/dump.sql'.format(db_host, db_username, db_password, db_database)
+            runcmd(cmd)
+            cmd = 'scp {}:/tmp/dump.sql {}'.format(db_host, database_log + '_' + str(test_id) + '_' + str(counter) + '.sql')
+            runcmd(cmd)
+        else:
+            cmd = 'mysqldump -u {} -p{} {} --result-file={}'.format(db_username, db_password, db_database, database_log + '_' + str(test_id) + '_' + str(counter) + '.sql')
+            runcmd(cmd)
+
 #    sock = socket.socket()
 #    sock.connect((application_ip, application_port))    
 #    sock.send(request)    
@@ -190,8 +270,6 @@ def num_edges(trie):
 
 def transform_tests(tests):
     trie = make_trie(tests)
-
- 
     
     T = []    
     S = [(trie, [])]
@@ -215,7 +293,7 @@ def transform_tests(tests):
             S.append((n[q], t))
         else:
             T = [t] + T
-            
+    
     return T
 
 def isolate_tests(tests):
@@ -225,6 +303,7 @@ def run_tests(tests):
     i = 1
     for test in tests:
         print ('Test {} out of {}'.format(i, len(tests)))
+        print test[0]
         i += 1
         
         runcmd('mysql -u {} -p{} {}'.format(db_username, db_password, db_database), input='reset query cache; flush tables;')
@@ -233,6 +312,7 @@ def run_tests(tests):
         checkpoint_time = 0
         restore_time = 0
         http_time = 0
+        counter = 1
         for request in test:
             if request.startswith('checkpoint'):
                 label = request.split(':')[1]
@@ -248,9 +328,10 @@ def run_tests(tests):
                 restore_time += (end_restore - start_restore)
             else:
                 start_http = time.time()
-                execute_request(request)
+                execute_request(request, i, counter)
                 end_http = time.time()
                 http_time += (end_http - start_http)
+                counter = counter + 1
         end_test = time.time()
         test_time = end_test - start_test
         output('time:{},checkpoint:{},restore:{},http:{}'.format(test_time, checkpoint_time, restore_time, http_time))
@@ -277,12 +358,22 @@ if __name__ == "__main__":
     global remove_duplicates, test_length, filter_extensions, \
         checkpointing, application_ip, application_port, proxy_ip, proxy_port, \
         db_host, db_username, db_password, db_database, number_of_result_tables, \
-        output_file, cookie
+        output_file, cookie, response_log, database_log, log, vm, cookies, current_cookie, \
+        labels_to_files, sessionSnapshotPath, timestamp
+        
+    timestamp = str(random.randint(1,10000000000))
         
     
     if len(sys.argv) != 2:
         print 'Usage:', sys.argv[0], '<config file>'
         sys.exit(-1)
+        
+    
+    cookies = {}
+    cookies['init'] = None
+    current_cookie = None
+    
+    labels_to_files = {}
         
         
     config_file = sys.argv[1]        
@@ -294,10 +385,27 @@ if __name__ == "__main__":
     filter_extensions  = config.get(TESTS_SECTION, 'FILTER_EXTENSIONS').split(',')
     cookie  = config.get(TESTS_SECTION, 'COOKIE')
     
+    fileFlag = config.getboolean(INSTRUMENTATION_SECTION, 'FILE_INSTRUMENTATION')
+    fileLogPath = config.get(INSTRUMENTATION_SECTION, 'ACCESSED_FILES_LOG_PATH')
+    
+    randomFlag = config.getboolean(INSTRUMENTATION_SECTION, 'RANDOM_INSTRUMENTATION')
+    randomSnapshotPath = config.get(INSTRUMENTATION_SECTION, 'RANDOM_SNAPSHOT_PATH')
+    randomSeedPath = config.get(INSTRUMENTATION_SECTION, 'RANDOM_SEED_PATH')
+    
+    timeFlag = config.getboolean(INSTRUMENTATION_SECTION, 'TIME_INSTRUMENTATION')
+    timeSnapshotPath = config.get(INSTRUMENTATION_SECTION, 'TIME_SNAPSHOT_PATH')  
+    timeSeedPath = config.get(INSTRUMENTATION_SECTION, 'TIME_SEED_PATH')
+    sessionSnapshotPath = config.get(INSTRUMENTATION_SECTION, 'RANDOM_SESSION_SNAPSHOT_PATH')
+        
+    
     isolation = config.getboolean(EXECUTION_SECTION, 'ISOLATION')
     optimize_tests = config.getboolean(EXECUTION_SECTION, 'OPTIMIZE_TESTS')
     checkpointing = config.getboolean(EXECUTION_SECTION, 'CHECKPOINTING')
-    output_filename = config.get(EXECUTION_SECTION, 'OUTPUT')    
+    vm = config.getboolean(EXECUTION_SECTION, 'VM')
+    output_filename = config.get(EXECUTION_SECTION, 'OUTPUT')
+    response_log = config.get(EXECUTION_SECTION, 'RESPONSE_LOG')
+    database_log = config.get(EXECUTION_SECTION, 'DATABASE_LOG')
+    log = config.getboolean(EXECUTION_SECTION, 'LOG')    
     
     application_ip = config.get(APPLICATION_SECTION, 'IP')
     application_port = config.getint(APPLICATION_SECTION, 'PORT')
@@ -327,6 +435,26 @@ if __name__ == "__main__":
         else:            
             tests = isolate_tests(tests)
             print 'Isolated tests size', sum([len(test) for test in tests])
+    if vm:
+        runcmd('VBoxManage startvm webcheck --type headless')
+        print "Sleep for 30 seconds"
+        time.sleep(30)
+    if checkpointing:
+        # set isolation level to read uncommitted (for proxy)
+        runcmd('mysql -u {} -p{} {}'.format(db_username, db_password, db_database), input='set global transaction isolation level read uncommitted;')    
+
+    if checkpoint:
+        checkpoint('start')
+            
+    run_tests(tests)
+        
+    if checkpoint:
+        restore('start')
+    if vm:
+        restore('init')
+        runcmd('VBoxManage controlvm webcheck poweroff')
+        
     
-    run_tests(tests)   
-    
+    if vm:
+        cmd = 'VBoxManage snapshot webcheck list | grep -v "init" | awk -F \' \' \'{print $4}\' | sed \'s/)//\' | tac | while read id; do echo $id; VBoxManage snapshot webcheck delete $id; done;'
+        runcmd(cmd)
