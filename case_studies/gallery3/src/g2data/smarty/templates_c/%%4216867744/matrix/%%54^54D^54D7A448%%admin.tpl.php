93acd35fa1d50ac09c0e5682f8cd2cc59a1ae615
Petar Tsankov <petar.tsankov@gmail.com>
2016-03-02 17:24:31 +0100

    removing source

--- case_studies/gallery3/src/g2data/smarty/templates_c/%%4216867744/matrix/%%54^54D^54D7A448%%admin.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php /* Smarty version 2.6.20, created on 2015-10-30 03:06:10
-         compiled from gallery:themes/matrix/templates/admin.tpl */ ?>
-<?php $_smarty_tpl_vars = $this->_tpl_vars;
-$this->_smarty_include(array('smarty_include_tpl_file' => "gallery:".($this->_tpl_vars['theme']['adminTemplate']), 'smarty_include_vars' => array('l10Domain' => $this->_tpl_vars['theme']['adminL10Domain'])));
-$this->_tpl_vars = $_smarty_tpl_vars;
-unset($_smarty_tpl_vars);
- ?>
